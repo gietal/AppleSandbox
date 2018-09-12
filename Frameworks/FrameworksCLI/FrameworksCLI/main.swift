f41e7d8b334741c597a96345fffd0c23d96283eb
Gieta Laksmana <gietal@microsoft.com>
2018-09-12 15:48:57 -0700

    a

---
+++ Frameworks/FrameworksCLI/FrameworksCLI/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FrameworksCLI
+//
+//  Created by gietal-dev on 11/09/18.
+//  Copyright © 2018 Microsoft. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
