964cb11156a195cb1f93da3daf674e59c0b5157f
Gieta Laksmana <gietal@microsoft.com>
2018-09-29 14:55:11 -0700

    coredata

---
+++ CoreData/CoreDataPlayground/CoreDataPlayground/AppDelegate.swift
@@ -0,0 +1,167 @@
+//
+//  AppDelegate.swift
+//  CoreDataPlayground
+//
+//  Created by gietal on 9/29/18.
+//  Copyright © 2018 gietal. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+        */
+        let container = NSPersistentContainer(name: "CoreDataPlayground")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                 
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving and Undo support
+
+    @IBAction func saveAction(_ sender: AnyObject?) {
+        // Performs the save action for the application, which is to send the save: message to the application's managed object context. Any encountered errors are presented to the user.
+        let context = persistentContainer.viewContext
+
+        // put data onto the scrathpad
+        let entity = updateDataEntity(getDataEntity())
+        
+        if !context.commitEditing() {
+            NSLog("\(NSStringFromClass(type(of: self))) unable to commit editing before saving")
+        }
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Customize this code block to include application-specific recovery steps.
+                let nserror = error as NSError
+                NSApplication.shared.presentError(nserror)
+            }
+        }
+    }
+
+    func windowWillReturnUndoManager(window: NSWindow) -> UndoManager? {
+        // Returns the NSUndoManager for the application. In this case, the manager returned is that of the managed object context for the application.
+        return persistentContainer.viewContext.undoManager
+    }
+
+    func applicationShouldTerminate(_ sender: NSApplication) -> NSApplication.TerminateReply {
+        // Save changes in the application's managed object context before the application terminates.
+        let context = persistentContainer.viewContext
+        
+        if !context.commitEditing() {
+            NSLog("\(NSStringFromClass(type(of: self))) unable to commit editing to terminate")
+            return .terminateCancel
+        }
+        
+        if !context.hasChanges {
+            return .terminateNow
+        }
+        
+        do {
+            try context.save()
+        } catch {
+            let nserror = error as NSError
+
+            // Customize this code block to include application-specific recovery steps.
+            let result = sender.presentError(nserror)
+            if (result) {
+                return .terminateCancel
+            }
+            
+            let question = NSLocalizedString("Could not save changes while quitting. Quit anyway?", comment: "Quit without saves error question message")
+            let info = NSLocalizedString("Quitting now will lose any changes you have made since the last successful save", comment: "Quit without saves error question info");
+            let quitButton = NSLocalizedString("Quit anyway", comment: "Quit anyway button title")
+            let cancelButton = NSLocalizedString("Cancel", comment: "Cancel button title")
+            let alert = NSAlert()
+            alert.messageText = question
+            alert.informativeText = info
+            alert.addButton(withTitle: quitButton)
+            alert.addButton(withTitle: cancelButton)
+            
+            let answer = alert.runModal()
+            if answer == .alertSecondButtonReturn {
+                return .terminateCancel
+            }
+        }
+        // If we got here, it is time to quit.
+        return .terminateNow
+    }
+    
+    func getDataEntity() -> NSManagedObject {
+        // get the persistent store context
+        let context = persistentContainer.viewContext
+        
+        // try to fetch
+        // if exist, return
+        // else create new entity
+        var fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "DataEntity")
+        fetchRequest.predicate = NSPredicate(format: "id == 1234")
+        fetchRequest.fetchLimit = 1
+        do {
+            if let entity = try context.fetch(fetchRequest).first {
+                return entity
+            }
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+        
+        print("creating a new entity instance")
+        
+        // create entity description
+        // this returns optional since entity name can be invalid
+        let entityDescription = NSEntityDescription.entity(forEntityName: "DataEntity", in: context)!
+        
+        // create an instance of the entity in the context from the description
+        let entity = NSManagedObject(entity: entityDescription, insertInto: context)
+        
+        return entity
+    }
+    
+    func updateDataEntity(_ entity: NSManagedObject) -> NSManagedObject {
+        // set id
+        entity.setValue("1234", forKey: "id")
+        
+        // set image as blob
+        if let blob = NSImage(named: "SampleBackground")?.tiffRepresentation {
+            entity.setValue(blob, forKey: "image")
+        }
+        
+        return entity
+    }
+}
+
---
+++ CoreData/CoreDataPlayground/CoreDataPlayground/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  CoreDataPlayground
+//
+//  Created by gietal on 9/29/18.
+//  Copyright © 2018 gietal. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+
+struct DataModel {
+    var id: String
+    var image: NSImage
+}
