e4e86e5bffd951d121fd2228523aac30f4bb5463
Gieta Laksmana <gietal@microsoft.com>
2019-01-22 15:26:37 -0800

    opaque sheet

---
+++ OpaqueSheet/OpaqueSheet/SheetViewController.swift
@@ -0,0 +1,47 @@
+//
+//  Sheet.swift
+//  OpaqueSheet
+//
+//  Created by gietal-dev on 1/22/19.
+//  Copyright © 2019 gietal-dev. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class SheetViewController: NSViewController {
+    convenience init() {
+        self.init(nibName: NSNib.Name(stringLiteral: "Sheet"), bundle: nil)
+    }
+    @IBAction func closeSheet(_ sender: Any) {
+        if let win = view.window {
+            win.sheetParent?.endSheet(win)
+        }
+    }
+    
+    override func viewWillAppear() {
+        // by default Appkit automatically adds an NSVisualEffectView
+        // behind our sheet view
+        // the structure of the sheet window then become this:
+        // window
+        // |- theme frame view
+        //    |- NSVisualEffectView
+        //    |- our view
+        //    |- other views that Appkit could possibly add
+        
+        // this will get all the views under the theme frame view
+        guard let siblings = view.superview?.subviews else {
+            return
+        }
+        
+        // this will get the visual effect view added by Appkit
+        guard let vfxView = siblings.first(where: { $0 is NSVisualEffectView }) as? NSVisualEffectView else {
+            return
+        }
+        
+        // this will make it so that the visual effect view stops blending
+        // its content with the beckground
+        vfxView.blendingMode = .withinWindow
+    }
+
+}
