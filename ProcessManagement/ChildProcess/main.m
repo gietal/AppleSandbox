84aa1414fe56c281d3643128f8f961bdbec6a3ab
Gieta Laksmana <gietal@microsoft.com>
2019-07-15 16:03:01 -0700

    play around with cfmessagaeport

--- ProcessManagement/ChildProcess/main.m
+++ ProcessManagement/ChildProcess/main.m
@@ -8,9 +8,33 @@
 
 #import <Foundation/Foundation.h>
 
+CFDataRef callback(CFMessagePortRef local, SInt32 msgid, CFDataRef data, void *info) {
+    NSLog(@"Child Process received message from parent: %d", msgid);
+    return NULL;
+}
+
+bool createPort() {
+    // DTT6RZZAQ.
+    CFStringRef port_name = CFSTR("DTT6RZZAQ.com.gietal.sandbox.group.port.client");
+    CFMessagePortRef port = CFMessagePortCreateLocal(kCFAllocatorDefault, port_name, &callback, NULL, NULL);
+    if (port == nil) {
+        NSLog(@"Child Process, port creation failed, port == nil");
+        return false;
+    } else {
+        NSLog(@"Child Process created local port");
+    }
+    CFMessagePortSetDispatchQueue(port, dispatch_get_main_queue());
+    return true;
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
+        
+        NSLog(@"Child process creating local port");
+//        if (!createPort())
+        {
+            return -1;
+        }
         NSLog(@"Child process counting to 10");
         
         int count = 1;
--- ProcessManagement/ParentProcess/main.m
+++ ProcessManagement/ParentProcess/main.m
@@ -8,6 +8,27 @@
 
 #import <Cocoa/Cocoa.h>
 
+CFDataRef callback(CFMessagePortRef local, SInt32 msgid, CFDataRef data, void *info)
+{
+    NSLog(@"parent received message from child: %d", msgid);
+    return NULL;
+}
+
+void create_local_port(void)
+{
+    CFStringRef port_name = CFSTR("DTT6RZZAQ.com.gietal.sandbox.group.port.server");
+    CFMessagePortRef port = CFMessagePortCreateLocal(kCFAllocatorDefault, port_name, &callback, NULL, NULL);
+    return;
+    CFMessagePortSetDispatchQueue(port, dispatch_get_main_queue());
+}
+
+void create_remote_port(void)
+{
+    CFStringRef port_name = CFSTR("DTT6RZZAQ.com.gietal.sandbox.group.port.client");
+    CFMessagePortRef port = CFMessagePortCreateRemote(kCFAllocatorDefault, port_name);
+}
+
 int main(int argc, const char * argv[]) {
+    create_local_port();
     return NSApplicationMain(argc, argv);
 }
